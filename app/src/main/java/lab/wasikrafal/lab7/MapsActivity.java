e57135cf1a1d7fa7cd1d378c17dbc4d62a92de5c
tasiuwasiu <tasiu24@gmail.com>
2018-05-01 21:58:48 +0200

    added items on nav menu and some listeners

--- app/src/main/java/lab/wasikrafal/lab7/MapsActivity.java
+++ app/src/main/java/lab/wasikrafal/lab7/MapsActivity.java
@@ -1,13 +1,20 @@
 package lab.wasikrafal.lab7;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SwitchCompat;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -21,6 +28,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     private GoogleMap mMap;
     private DrawerLayout mDrawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -28,15 +36,74 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         mDrawerLayout = findViewById(R.id.drawer_layout);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(toolbar);
         ActionBar actionbar = getSupportActionBar();
-        actionbar.setDisplayHomeAsUpEnabled(true);
-        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        if(actionbar!=null) {
+            actionbar.setDisplayHomeAsUpEnabled(true);
+            actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        }
+
+        initNavigationMenu();
+
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+    }
+
+    private void initNavigationMenu()
+    {
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                        int id = menuItem.getItemId();
+
+                        if(id == R.id.nav_first_road)
+                        {
+                            mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                        }
+
+
+                        mDrawerLayout.closeDrawers();
+                        return true;
+                    }
+                });
+
+        Menu navigationMenu = navigationView.getMenu();
+        MenuItem satelliteItem = navigationMenu.findItem(R.id.nav_satellite);
+        SwitchCompat satelliteSwitch = (SwitchCompat) satelliteItem.getActionView().findViewById(R.id.nav_switch);
+        satelliteSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+        {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b)
+            {
+                Toast.makeText(MapsActivity.this, "is enabled frs: " + b , Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        MenuItem positionItem = navigationMenu.findItem(R.id.nav_position);
+        SwitchCompat positionSwitch = (SwitchCompat) positionItem.getActionView().findViewById(R.id.nav_switch);
+        positionSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+        {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b)
+            {
+                try
+                {
+                    mMap.setMyLocationEnabled(b);
+                }
+                catch (SecurityException e)
+                {
+                    Toast.makeText(MapsActivity.this, "GPS error" , Toast.LENGTH_LONG).show();
+                    compoundButton.setChecked(false);
+                }
+                //Toast.makeText(MapsActivity.this, "is enabled sec: " + b , Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 
